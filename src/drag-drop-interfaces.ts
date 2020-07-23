54ddf5e875614b027227fe919ab82defc3f759aa
Max Hardy <max@hardy.work>
2020-07-23 14:59:35 +0100

    Splitting code to multiple files

---
+++ src/drag-drop-interfaces.ts
@@ -0,0 +1,12 @@
+namespace App {
+  export interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+  }
+
+  export interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+  }
+}
---
+++ src/project-model.ts
@@ -0,0 +1,16 @@
+namespace App {
+  export enum ProjectStatus {
+    Active,
+    Finished,
+  }
+
+  export class Project {
+    constructor(
+      public id: string,
+      public title: string,
+      public description: string,
+      public people: number,
+      public status: ProjectStatus,
+    ) {}
+  }
+}
